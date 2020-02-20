7ee97b4a6cc81b26f71bbb156927494a83bc59dc
mmiller7 <mmiller7@mail.umw.edu>
2020-02-19 20:52:21 -0500

    Create telegram_bot_send_message_helper.py

---
+++ laundry/telegram_bot_send_message_helper.py
@@ -0,0 +1,29 @@
+# Filename: telegram_bot_send_message_helper.py
+# 
+# Source:
+# https://community.home-assistant.io/t/help-how-to-make-a-dynamic-list-of-integers-variable-not-working/
+#
+# Usage from service call:
+# 
+#       - service: python_script.telegram
+#        data_template:
+#          target: "{{ states.variable.list_of_user_ids_to_send_to.state }}"
+#          message: The message to send to all of them.
+#
+# Requires:
+# The expected bot is integration named "telegram_bot"
+
+target = data.get('target', '')
+message = data.get('message', '')
+if target and message:
+    if ',' in target:
+        targets = [ int(v.strip()) for v in target.split(',') ]
+    else:
+        targets = [ int(target) ]
+
+    service_data = {
+        'target': targets,
+        'message': message,
+    }
+
+    hass.services.call('telegram_bot', 'send_message', service_data)
