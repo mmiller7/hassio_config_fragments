f76c5e49c3d81950751d81fecc0df0185f5b1d1d
mmiller7 <mmiller7@mail.umw.edu>
2020-05-15 11:00:41 -0400

    Improved filtering, fixed file cleanup if run more than once

--- RTL_433 to MQTT Sensors/hassio_accurite_generator.sh
+++ RTL_433 to MQTT Sensors/hassio_accurite_generator.sh
@@ -2,6 +2,8 @@
 s_f="output/generated_sensors.yaml"
 bs_f="output/generated_binary_sensors.yaml"
 p_f="output/generated_combined_package.yaml"
+f_f="output/generated_sensors_filters.yaml"
+fp_f="output/generated_sensors_filters_package.yaml"
 topic_base="homeassistant/sensor/rtl433/Acurite_tower_sensor"
 
 # List of sensors
@@ -18,11 +20,16 @@ temp_max_C=100
 echo '' > $s_f
 echo '' > $bs_f
 echo '' > $p_f
+echo '' > $f_f
+echo '' > $fp_f
 
+# here we go
 echo ""
 echo "Sensor filename: $s_f"
 echo "Binary Sensor filename: $bs_f"
 echo "package filename: $p_f"
+echo "Filter filename: $f_f"
+echo "Filter package filename: $fp_f"
 echo "MQTT Topic Base: $topic_base"
 echo ""
 echo "Generating YAML..."
@@ -30,61 +37,91 @@ echo "Generating YAML..."
 # Replace the items in the "for" line with "<decimal_id>/Name" for each sensor
 for i in "${sensor_list[@]}"
 do
-        # compute variables from sensor list
-        id=`echo $i | awk -F '/' '{ print $1 }'`
-        name=`echo $i | awk -F '/' '{ print $2 }'`
-        unique_id=`echo $name | tr '[:upper:]' '[:lower:]' | sed 's/ /_/g'`
+	# compute variables from sensor list
+	id=`echo $i | awk -F '/' '{ print $1 }'`
+	name=`echo $i | awk -F '/' '{ print $2 }'`
+	unique_id=`echo $name | tr '[:upper:]' '[:lower:]' | sed 's/ /_/g'`
 
-        # debug info
-        echo "$name -> $id"
+	# debug info
+	echo "$name -> $id"
 
-        # write out data to file
-        echo "  # $name Sensor" >> $s_f
-        echo "  - platform: mqtt" >> $s_f
-        echo "    name: \"$name Temperature\"" >> $s_f
-        echo "    unique_id: \"${unique_id}_temperature\"" >> $s_f
-        echo "    state_topic: \"$topic_base/$id\"" >> $s_f
-        echo "    unit_of_measurement: '°C'" >> $s_f
-        echo "    value_template: >-" >> $s_f
-        echo "      {% if value_json.temperature_C > $temp_min_C and value_json.temperature_C < $temp_max_C %}" >> $s_f
-        echo "        {{ value_json.temperature_C }}" >> $s_f
-        echo "      {% else %}" >> $s_f
-        echo "        {{ states.sensor.${unique_id}_temperature.state }}" >> $s_f
-        echo "      {% endif %}" >> $s_f
-
-
-        echo "  - platform: mqtt" >> $s_f
-        echo "    name: \"$name Humidity\"" >> $s_f
-        echo "    unique_id: \"${unique_id}_humidity\"" >> $s_f
-        echo "    state_topic: \"$topic_base/$id\"" >> $s_f
-        echo "    unit_of_measurement: '%'" >> $s_f
-        echo "    value_template: >-" >> $s_f
-        echo "      {% if value_json.humidity >= 0 and value_json.humidity <= 100 %}" >> $s_f
-        echo "        {{ value_json.humidity }}" >> $s_f
-        echo "      {% else %}" >> $s_f
-        echo "        {{ states.sensor.${unique_id}_humidity.state }}" >> $s_f
-        echo "      {% endif %}" >> $s_f
-        echo "" >> $s_f
+	# write out data to file
+	echo "  # $name Sensor" >> $s_f
+	echo "  - platform: mqtt" >> $s_f
+	echo "    name: \"$name Temperature\"" >> $s_f
+	echo "    unique_id: \"${unique_id}_temperature\"" >> $s_f
+	echo "    force_update: true" >> $s_f
+	echo "    state_topic: \"$topic_base/$id\"" >> $s_f
+	echo "    unit_of_measurement: '°C'" >> $s_f
+	echo "    value_template: >-" >> $s_f
+	echo "      {% if value_json.temperature_C > $temp_min_C and value_json.temperature_C < $temp_max_C %}" >> $s_f
+	echo "        {{ value_json.temperature_C }}" >> $s_f
+	echo "      {% else %}" >> $s_f
+	echo "        {{ states.sensor.${unique_id}_temperature.state }}" >> $s_f
+	echo "      {% endif %}" >> $s_f
 
 
+	echo "  - platform: mqtt" >> $s_f
+	echo "    name: \"$name Humidity\"" >> $s_f
+	echo "    unique_id: \"${unique_id}_humidity\"" >> $s_f
+	echo "    force_update: true" >> $s_f
+	echo "    state_topic: \"$topic_base/$id\"" >> $s_f
+	echo "    unit_of_measurement: '%'" >> $s_f
+	echo "    value_template: >-" >> $s_f
+	echo "      {% if value_json.humidity >= 0 and value_json.humidity <= 100 %}" >> $s_f
+	echo "        {{ value_json.humidity }}" >> $s_f
+	echo "      {% else %}" >> $s_f
+	echo "        {{ states.sensor.${unique_id}_humidity.state }}" >> $s_f
+	echo "      {% endif %}" >> $s_f
+	echo "" >> $s_f
 
-        echo "  # $name Sensor" >> $bs_f
-        echo "  - platform: mqtt" >> $bs_f
-        echo "    name: \"$name Sensor Battery Low\"" >> $bs_f
-        echo "    unique_id: \"${unique_id}_sensor_battery_low\"" >> $bs_f
-        echo "    state_topic: \"$topic_base/$id\"" >> $bs_f
-        echo "    payload_on: \"1\"" >> $bs_f
-        echo "    payload_off: \"0\"" >> $bs_f
-        echo "    value_template: >-" >> $bs_f
-        echo "      {% if value_json.battery_low == 0 or value_json.battery_low == 1 %}" >> $bs_f
-        echo "        {{ value_json.battery_low }}" >> $bs_f
-        echo "      {% else %}" >> $bs_f
-        echo "        {{ states.sensor.${unique_id}_battery_low.state }}" >> $bs_f
-        echo "      {% endif %}" >> $bs_f
-        echo "" >> $bs_f
+
+
+	echo "  # $name Sensor" >> $bs_f
+	echo "  - platform: mqtt" >> $bs_f
+	echo "    name: \"$name Sensor Battery Low\"" >> $bs_f
+	echo "    unique_id: \"${unique_id}_sensor_battery_low\"" >> $bs_f
+	echo "    state_topic: \"$topic_base/$id\"" >> $bs_f
+	echo "    payload_on: \"1\"" >> $bs_f
+	echo "    payload_off: \"0\"" >> $bs_f
+	echo "    value_template: >-" >> $bs_f
+	echo "      {% if value_json.battery_low == 0 or value_json.battery_low == 1 %}" >> $bs_f
+	echo "        {{ value_json.battery_low }}" >> $bs_f
+	echo "      {% else %}" >> $bs_f
+	echo "        {{ states.sensor.${unique_id}_battery_low.state }}" >> $bs_f
+	echo "      {% endif %}" >> $bs_f
+	echo "" >> $bs_f
+
+
+
+	echo "  - platform: filter" >> $f_f
+	echo "    name: \"$name Temperature Filtered\"" >> $f_f
+	# this does not support unique_id
+	# entity_id is the sensor to use for input to the filter
+	echo "    entity_id: sensor.${unique_id}_temperature" >> $f_f
+        echo "    filters:" >> $f_f
+        echo "      - filter: outlier" >> $f_f
+        echo "        window_size: 3" >> $f_f
+        echo "        radius: 10.0" >> $f_f
+        echo "      - filter: time_simple_moving_average" >> $f_f
+        echo "        window_size: 00:01" >> $f_f
+        echo "        precision: 1" >> $f_f
+
+        echo "  - platform: filter" >> $f_f
+	echo "    name: \"$name Humidity Filtered\"" >> $f_f
+	# this does not support unique_id
+	# entity_id is the sensor to use for input to the filter
+	echo "    entity_id: sensor.${unique_id}_humidity" >> $f_f
+        echo "    filters:" >> $f_f
+        echo "      - filter: outlier" >> $f_f
+        echo "        window_size: 3" >> $f_f
+        echo "        radius: 10.0" >> $f_f
+        echo "      - filter: time_simple_moving_average" >> $f_f
+        echo "        window_size: 00:01" >> $f_f
+        echo "        precision: 1" >> $f_f
+        echo "" >> $f_f
 
 done
-
 
 echo ""
 echo "Merging generated data into package..."
@@ -99,5 +136,11 @@ echo "" >> $p_f
 echo "# Battery Low Warning Sensors" >> $p_f
 echo "binary_sensor:" >> $p_f
 cat $bs_f >> $p_f
+
+echo "# Sensor Filter Package File" >> $fp_f
+echo "" >> $fp_f
+echo "sensor:" >> $fp_f
+echo "" >> $fp_f
+cat $f_f >> $fp_f
 
 echo "Done."
