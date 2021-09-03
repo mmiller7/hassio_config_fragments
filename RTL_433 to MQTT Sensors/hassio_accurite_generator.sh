2ece62b96ab094a588f7f66f58dc7316dd89268b
mmiller7 <mmiller7@mail.umw.edu>
2021-09-03 14:02:23 -0400

    Update hassio_accurite_generator.sh

--- RTL_433 to MQTT Sensors/hassio_accurite_generator.sh
+++ RTL_433 to MQTT Sensors/hassio_accurite_generator.sh
@@ -6,16 +6,20 @@ f_f="output/generated_sensors_filters.yaml"
 fp_f="output/generated_sensors_filters_package.yaml"
 lu_f="output/generated_sensors_last_updated.yaml"
 lup_f="output/generated_sensors_last_updated_package.yaml"
-topic_base="homeassistant/sensor/rtl433/Acurite_tower_sensor"
+sn_f="output/generated_sensors_signal.yaml"
+snp_f="output/generated_sensors_signal_package.yaml"
+#topic_base="homeassistant/sensor/rtl433/Acurite_tower_sensor"
+topic_base="homeassistant/sensor/rtl433/Acurite-Tower"
 
 # List of sensors
 sensor_list=( "1234/Outside Front" "5678/Outside Rear" "4321/Downstairs Hall" "8765/Upstairs Hall" )
+
 
 # Indoor sensors spec says -20 to +70C
 # Outdoor sensors spec says -40C to +70C
 # but most bad high values seem WAY high hundreds
 temp_min_C=-20
-temp_max_C=100
+temp_max_C=70
 
 
 echo '' > $s_f
@@ -25,6 +29,8 @@ echo '' > $f_f
 echo '' > $fp_f
 echo '' > $lu_f
 echo '' > $lup_f
+echo '' > $sn_f
+echo '' > $snp_f
 
 # here we go
 echo ""
@@ -71,6 +77,7 @@ do
 	echo "  - platform: mqtt" >> $s_f
 	echo "    name: \"$name Humidity\"" >> $s_f
 	echo "    unique_id: \"${unique_id}_humidity\"" >> $s_f
+	echo "    icon: hass:water-percent" >> $s_f
 	echo "    force_update: true" >> $s_f
 	echo "    state_topic: \"$topic_base/$id\"" >> $s_f
 	echo "    unit_of_measurement: '%'" >> $s_f
@@ -88,14 +95,21 @@ do
 	echo "  - platform: mqtt" >> $bs_f
 	echo "    name: \"$name Sensor Battery Low\"" >> $bs_f
 	echo "    unique_id: \"${unique_id}_sensor_battery_low\"" >> $bs_f
+	echo "    device_class: battery" >> $bs_f
 	echo "    state_topic: \"$topic_base/$id\"" >> $bs_f
-	echo "    payload_on: \"1\"" >> $bs_f
-	echo "    payload_off: \"0\"" >> $bs_f
+#	echo "    payload_on: \"1\"" >> $bs_f
+#	echo "    payload_off: \"0\"" >> $bs_f
+	echo "    payload_on: \"0\"" >> $bs_f
+	echo "    payload_off: \"1\"" >> $bs_f
 	echo "    value_template: >-" >> $bs_f
-	echo "      {% if value_json.battery_low == 0 or value_json.battery_low == 1 %}" >> $bs_f
-	echo "        {{ value_json.battery_low }}" >> $bs_f
+#	echo "      {% if value_json.battery_low == 0 or value_json.battery_low == 1 %}" >> $bs_f
+#	echo "        {{ value_json.battery_low }}" >> $bs_f
+#	echo "      {% else %}" >> $bs_f
+#	echo "        {{ states.sensor.${unique_id}_battery_low.state }}" >> $bs_f
+	echo "      {% if value_json.battery_ok == 1 or value_json.battery_ok == 0 %}" >> $bs_f
+	echo "        {{ value_json.battery_ok }}" >> $bs_f
 	echo "      {% else %}" >> $bs_f
-	echo "        {{ states.sensor.${unique_id}_battery_low.state }}" >> $bs_f
+	echo "        {{ states.sensor.${unique_id}_battery_ok.state }}" >> $bs_f
 	echo "      {% endif %}" >> $bs_f
 	echo "" >> $bs_f
 
@@ -138,15 +152,30 @@ do
 	echo "    sensors:" >> $lu_f
 	echo "      ${unique_id}_sensor_age:" >> $lu_f
 	echo "        friendly_name: \"$name Sensor Age\"" >> $lu_f
-	echo "        entity_id: sensor.time" >> $lu_f
+#	echo "        entity_id: sensor.time" >> $lu_f
 	echo "        value_template: >-" >> $lu_f
 	echo "          {% if states.sensor.${unique_id}_temperature.last_changed > states.sensor.${unique_id}_humidity.last_changed %}" >> $lu_f
-	echo "            {{ (states.sensor.time.last_changed - states.sensor.${unique_id}_temperature.last_changed).total_seconds() | round(0) }}" >> $lu_f
+#	echo "            {{ (states.sensor.time.last_changed - states.sensor.${unique_id}_temperature.last_changed).total_seconds() | round(0) }}" >> $lu_f
+	echo "            {{ (now() - states.sensor.${unique_id}_temperature.last_changed).total_seconds() | round(0) }}" >> $lu_f
 	echo "          {% else %}" >> $lu_f
-	echo "            {{ (states.sensor.time.last_changed - states.sensor.${unique_id}_humidity.last_changed).total_seconds() | round(0) }}" >> $lu_f
+#	echo "            {{ (states.sensor.time.last_changed - states.sensor.${unique_id}_humidity.last_changed).total_seconds() | round(0) }}" >> $lu_f
+	echo "            {{ (now() - states.sensor.${unique_id}_humidity.last_changed).total_seconds() | round(0) }}" >> $lu_f
 	echo "          {% endif %}" >> $lu_f
 	echo "        unit_of_measurement: \"Seconds\"" >> $lu_f
 	echo "" >> $lu_f
+
+
+	# custom signal sensor counter for watchdog or other stat use
+
+	echo "  - platform: mqtt" >> $sn_f
+	echo "    name: \"$name Sensor SNR\"" >> $sn_f
+	echo "    unique_id: \"${unique_id}_sensor_snr\"" >> $sn_f
+	echo "    icon: mdi:signal" >> $sn_f
+	echo "    state_topic: \"$topic_base/$id\"" >> $sn_f
+	echo "    unit_of_measurement: 'dB'" >> $sn_f
+	echo "    value_template: >-" >> $sn_f
+	echo "      {{ value_json.snr }}" >> $sn_f
+	echo "" >> $sn_f
 
 done
 
@@ -175,5 +204,11 @@ echo "" >> $lup_f
 echo "sensor:" >> $lup_f
 echo "" >> $lup_f
 cat $lu_f >> $lup_f
+
+echo "# Sensor Signal Package File" >> $snp_f
+echo "" >> $snp_f
+echo "sensor:" >> $snp_f
+echo "" >> $snp_f
+cat $sn_f >> $snp_f
 
 echo "Done."
